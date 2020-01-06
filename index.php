ee3e5f9d630dc7fc0dfb6f6869be7d1e297c36f1
anitokigrab <59564902+anitokigrab@users.noreply.github.com>
2020-01-06 20:42:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 include 'func.php';
-if(isset($_GET['query'])){
-if(!empty($_GET['query'])){
-header('Location: '.$dir.'/search/'.hap($_GET['query']).'.html');
-}else{
-header('Location: /');
-}}
-$q=$_GET['s'];
-if(!empty($_GET['p'])){
-$p=$_GET['p'];
-}else{
-$p = 1;
-}
 if(!empty($q)){
 $title = 'Download '.hap1($q).' – Anime Shinoa';
 $result = 'Result for '.hap1($q);
@@ -20,13 +8,20 @@ $querys = $q;
 }else{
 $title = ''.$dir2.' – '.$dir3.'';
 }
-include 'head.php';
-echo '<div class="ijo"><h3>'.$result.'</h3></div>';
 
-$grab = ngegrab('https://anitoki.com');
+$anitoki = ngegrab('https://anitoki.com');
+$jupok = explode('<!DOCTYPE html PUBLIC', $anitoki);
+$posting = explode('<div class="rseries right"><div class="rvad"><h1><span>Latest Update</span></h1>', $jupok[1]);
+$posting = explode('<div class="rekomf"></div>', $posting[1]);
+// $time = explode('<tr><td class="p_s">Time</td><td class="p_m">', $pecah[1]);
+// $time = explode('</td></tr>', $time[1]);
+// $pecaho = explode('<head>', $grogol);
+// $title = explode('<title>', $pecaho[1]);
+// $title = explode(' - GROGOLDRIVE LINK', $title[1]);
+// include 'head.php';
+echo '<div class="ijo"><h3>LIST UPDATE</h3></div>';
 
-echo ''.grab.'';
-else{
-echo '<div class="group"><p align="center"><b>Masih Error Njing!!</b></p></div>';
-include 'foot.php';
+
+echo ''.$posting[1].'';
+// include 'foot.php';
 ?>
