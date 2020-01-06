09fa284b11e5f74ef13d6525eb34ab3e59000d41
anitokigrab <59564902+anitokigrab@users.noreply.github.com>
2020-01-07 00:11:36 +0700

    Update media.php

--- test/media.php
+++ test/media.php
@@ -105,9 +105,9 @@ echo '<div class="row justify-content-center">
 
 <div class="input-group">
 <div class="custom-file">
-<form method="post" enctype="multipart/form-data" type="file" class="custom-file-input" id="uploader" aria-describedby="uploader"><label class="custom-file-label" for="file">Choose file</label></div>
+<form method="post" enctype="multipart/form-data" type="file" class="custom-file-input" id="uploader" aria-describedby="uploader"><label class="custom-file-label" for="uploader">Choose file</label></div>
 <div class="input-group-append"> 
-<input name="_upl" class="btn btn-secondary" type="submit" id="_upl" value="Upload File"> </div> </div></form>
+<input name="_upl" class="btn btn-secondary" type="submit" id="_upl" value="Upload File"> </div></form> </div>
 
 </div>
 </div>
