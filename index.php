aee4520bc94705b63d2c2460cd398dae2cd62510
anitokigrab <59564902+anitokigrab@users.noreply.github.com>
2020-01-06 20:52:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,17 +9,17 @@ $querys = $q;
 $title = ''.$dir2.' – '.$dir3.'';
 }
 
-$anitoki = ngegrab('https://anitoki.com');
-$jupok = explode('<!DOCTYPE html PUBLIC', $anitoki);
-$posting = explode('<div class="rseries right"><div class="rvad"><h1><span>Latest Update</span></h1>', $jupok[1]);
-$posting = explode('<div class="rekomf"></div>', $posting[1]);
+$anitoki = ngegrab('https://anitoki.com/');
+$jupok = explode('<h2 class="blogname">Anitoki</h2>', $anitoki);
+$posting = explode('<div class="rseries right">', $jupok[1]);
+$posting = explode('<div class="rekomf">', $posting[1]);
 // $time = explode('<tr><td class="p_s">Time</td><td class="p_m">', $pecah[1]);
 // $time = explode('</td></tr>', $time[1]);
 // $pecaho = explode('<head>', $grogol);
 // $title = explode('<title>', $pecaho[1]);
 // $title = explode(' - GROGOLDRIVE LINK', $title[1]);
 // include 'head.php';
-echo '<div class="ijo"><h3>LIST UPDATE</h3></div>';
+
 
 
 echo ''.$posting[1].'';
