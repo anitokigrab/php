065ac636cfd8f21bbf4577f9c4650736ee1e83f6
anitokigrab <59564902+anitokigrab@users.noreply.github.com>
2020-01-06 20:55:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ $title = ''.$dir2.' – '.$dir3.'';
 }
 
 $anitoki = ngegrab('https://anitoki.com/');
-$jupok = explode('<h2 class="blogname">Anitoki</h2>', $anitoki);
-$posting = explode('<div class="rseries right">', $jupok[1]);
-$posting = explode('<div class="rekomf">', $posting[1]);
+$postnya = cut('<div class="rseries right">','<div class="rekomf">', $anitoki);
+// $posting = explode('<div class="rseries right">', $jupok[1]);
+// $posting = explode('<div class="rekomf">', $posting[1]);
 // $time = explode('<tr><td class="p_s">Time</td><td class="p_m">', $pecah[1]);
 // $time = explode('</td></tr>', $time[1]);
 // $pecaho = explode('<head>', $grogol);
@@ -22,6 +22,6 @@ $posting = explode('<div class="rekomf">', $posting[1]);
 
 
 
-echo ''.$posting[1].'';
+echo ''.$postnya[1].'';
 // include 'foot.php';
 ?>
